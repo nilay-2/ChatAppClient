440649ec9151d4af0324bffca900ea15fa35a1dd
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-07-25 19:09:23 +0530

    added render.com deployment comments

--- src/shared/utils/url.js
+++ src/shared/utils/url.js
@@ -4,6 +4,6 @@ export const devFrontEndUrl = "http://localhost:3000";
 // export const prodBackEndUrl = "https://chatsphereserver.up.railway.app";
 // export const prodFrontEndUrl = "https://chatsphereclient.netlify.app";
 
+// for render.com
 export const prodBackEndUrl = "https://chatserver-bs6i.onrender.com";
 export const prodFrontEndUrl = "https://chatsphereclient.netlify.app";
-// added url for render.com
