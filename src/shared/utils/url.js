6032769fe655fe448f0f8af1bf7956c75814d317
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-07-31 22:38:41 +0530

    deployed to adaptable.io

--- src/shared/utils/url.js
+++ src/shared/utils/url.js
@@ -1,9 +1,13 @@
 export const devBackEndUrl = "http://localhost:5000";
 export const devFrontEndUrl = "http://localhost:3000";
 
-export const prodBackEndUrl = "https://chatsphereserver.up.railway.app";
-export const prodFrontEndUrl = "https://chatsphereclient.netlify.app";
+// export const prodBackEndUrl = "https://chatsphereserver.up.railway.app";
+// export const prodFrontEndUrl = "https://chatsphereclient.netlify.app";
 
 // for render.com
 // export const prodBackEndUrl = "https://chatserver-bs6i.onrender.com";
 // export const prodFrontEndUrl = "https://chatsphereclient.netlify.app";
+
+// for adaptable.io
+export const prodBackEndUrl = "https://chatsphereserver.adaptable.app";
+export const prodFrontEndUrl = "https://chatsphereclient.netlify.app";
