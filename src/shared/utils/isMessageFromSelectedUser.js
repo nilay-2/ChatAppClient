4bd69959be1a5d8e35a729fc0003fa5290b3886c
Nilay Pophalkar <nilaypophalkar@gmail.com>
2024-01-02 17:48:47 +0530

    optimized the function for sending direct chat notification

--- src/shared/utils/isMessageFromSelectedUser.js
+++ src/shared/utils/isMessageFromSelectedUser.js
@@ -6,25 +6,34 @@ export const isMessageFromSelectedEntity = (
 ) => {
   switch (chatType) {
     case "DIRECT":
-      const arrayOfparticipants = [];
+      // const arrayOfparticipants = [];
       // console.log(currUser, selectedEntity, data);
       // get current logged in user and the selected chat user
       const currUserId = currUser.userDetails?._id;
       const selectedEntityId = selectedEntity?.id;
 
       // make a array which includes the above two user's IDs
-      arrayOfparticipants.push(currUserId);
-      arrayOfparticipants.push(selectedEntityId);
+      // arrayOfparticipants.push(currUserId);
+      // arrayOfparticipants.push(selectedEntityId);
 
       // get the receiver and author IDs from the message data
       const authorId = data.author?._id;
-      const receiverId = data?.receiver;
+      // const receiverId = data?.receiver;
 
       // return true if the incoming message's author and receiver are either the logged in user and the selected chat user
-      return (
-        arrayOfparticipants.includes(authorId) &&
-        arrayOfparticipants.includes(receiverId)
-      );
+      // return (
+      //   arrayOfparticipants.includes(authorId) &&
+      //   arrayOfparticipants.includes(receiverId)
+      // );
+
+      // the message is going to be sent to both the sender and the reciever
+
+      if (currUserId === authorId) return true; // condition for sender
+
+      if (selectedEntityId === authorId) return true; // condition for receiver
+
+      if (selectedEntity !== authorId) return false; // condition for receiver
+
     case "GROUP":
       // get current selected group id
       const groupId = selectedEntity?._id;
